db87350be57121ab37047952333fc88ff167a647
Shreyas21A <shreyas2002alv@gmail.com>
2025-06-30 13:50:58 +0530

    Add basic CSS styling for auth forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import './App.css';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<h1>Home</h1>} />
       </Routes>
     </Router>
